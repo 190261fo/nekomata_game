882cf2caff23b6c5fa379e86e548774534fdb310
190261fo <69778270+190261fo@users.noreply.github.com>
2020-10-08 15:19:46 +0900

    1008_title遷移など
    タイトル画面、タイトルからはじめるを押してゲーム画面への遷移

---
+++ Assets/Script/SceneChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //シーン切り替えに使用するライブラリ
+
+public class SceneChange_Title : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
