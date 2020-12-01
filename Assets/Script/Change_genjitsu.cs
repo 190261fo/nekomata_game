d061f0e5aa6437d168b0bfc8b01efbe68854eace
190261fo <69778270+190261fo@users.noreply.github.com>
2020-12-01 12:49:03 +0900

    1201_1236_パッケージ結合前

--- Assets/Script/Change_genjitsu.cs
+++ Assets/Script/Change_genjitsu.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; //シーン切り替えに使用するライブラリ
 
-public class Change_main : MonoBehaviour
+public class Change_genjitsu : MonoBehaviour
 {
     public void OnClickStartButton()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("GenjitsuScene");
     }
 }
