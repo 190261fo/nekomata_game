b51a74dafe5174b464eeca17dcbb07da8a36083b
190261fo <69778270+190261fo@users.noreply.github.com>
2020-12-03 12:21:25 +0900

    1203_1220_scenechangescript

--- Assets/Script/FadePractice.cs
+++ Assets/Script/FadePractice.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement; //シーン切り替えに使用するライブラリ
+
 
 public class FadePractice : MonoBehaviour
 {
     public Fade fade;
+    float step_time;
 
     private void Update()
     {
+        // Fadeおためし
         if (Input.GetKeyDown(KeyCode.I))
         {
             fade.FadeIn(1.0f, () => print("フェードイン完了"));
@@ -15,5 +20,24 @@ public class FadePractice : MonoBehaviour
         {
             fade.FadeOut(1.0f, () => print("フェードアウト完了"));
         }
+
+        // ミニゲーム1 へ遷移(仮)
+        else if (Input.GetKeyDown("1"))
+        {
+            fade.FadeIn(1.0f, () => {
+                print("フェードイン完了");
+                // 経過時間をカウント
+                step_time += Time.deltaTime;
+                if (step_time >= 1.5f)
+                {
+                    SceneManager.LoadScene("TyouchinLightsOut");
+                }
+                fade.FadeOut(2.5f, () => print("ミニゲーム1 起動"));
+            });
+            
+        }
+
+        
+
     }
 }
