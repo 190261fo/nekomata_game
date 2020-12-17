22766bac8f2449fb1facb48c0d5ace5b15a4228d
190261fo <69778270+190261fo@users.noreply.github.com>
2020-12-17 12:02:25 +0900

    1217_ngashima_data

---
+++ Assets/Script/FungusMessenger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FungusMessenger : MonoBehaviour
+{
+    // 表示・非表示の対象
+    public GameObject SetObj;
+    // 上記用のフラグ
+    public Boolean FlagActive = true;
+
+    public Fungus.Flowchart flowchart = null;
+    public string message = "";
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (flowchart && collision.gameObject.tag == "Player")
+        {
+            flowchart.SendFungusMessage(message);
+        }
+    }
+
+    void Update()
+    {
+        // FlagActiveがtrueならオブジェクト表示
+        if (FlagActive)
+        {
+            SetObj.SetActive(true);
+        } else
+        {
+            SetObj.SetActive(false);
+        }
+    }
+}
